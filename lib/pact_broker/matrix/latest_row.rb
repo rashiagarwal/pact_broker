bad7de8bc0e8355921ab3573282043fed56e25a0
Beth Skurrie <beth@bethesque.com>
2018-05-17 14:23:50 +1000

    feat: keep track of latest pact revision in table rather than calculating it

--- lib/pact_broker/matrix/latest_row.rb
+++ lib/pact_broker/matrix/latest_row.rb
@@ -3,6 +3,7 @@ require 'pact_broker/matrix/row'
 module PactBroker
   module Matrix
     # Latest pact revision for each consumer version => latest verification for each provider version
+    # TODO is this even used? Delete? The view is used by head_matrix.
     class LatestRow < Row
       set_dataset(:latest_matrix_for_consumer_version_and_provider_version)
     end
